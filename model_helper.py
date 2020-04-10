c2f7e693b6dc8c1cc0e539b8d270570c0298916f
ishaandey <ishaan.dey8@gmail.com>
2020-04-10 11:09:40 -0400

    Major SIR model edits

--- model_helper.py
+++ model_helper.py
@@ -34,41 +34,20 @@ def get_r2(popt, how, x, y):
     r2 = 1 - (ss_res / ss_tot)
     return r2
 
-def regress(x, y, how='exponential', verbose=True):
-    try:
-        if how.lower() == 'exponential':
-            popt, pcov = curve_fit(exponential, x, y, maxfev=10000, bounds=([0,0,-100],[100,0.9,100]))
-            y_pred = exponential(x, *popt)
-        elif how.lower() == 'linear':
-            popt, pcov = curve_fit(linear, x, y, maxfev=10000)  
-            y_pred = linear(x, *popt)
-        elif how.lower() == 'logistic':   
-            popt, pcov = curve_fit(logistic, x, y, maxfev=10000)   
-            y_pred = logistic(x, *popt)
-        elif how.lower() == 'lowess':
-            y_pred = sm.nonparametric.lowess(y, x, frac=7/len(x), it=0, return_sorted=False)
-            popt = None
-        else:
-            print('regress(how=) currently accepts exponential, linear, logistic, and lowess!')
-            return None
-    except RuntimeError:
-        print('Unable to fit {} curve to data'.format(how))
-        return None
-
+def regress_lowess(x, y, verbose=True):
+    y_pred = sm.nonparametric.lowess(y, x, frac=7/len(x), it=0, return_sorted=False)
     r2 = r2_score(y, y_pred)    
     
     if verbose:
-        print('R2 for {h}: %2.5f'.format(h=how)%r2)
-        if popt is not None:
-            print('Model Params: {}'.format(popt))
+        print('R2 for LOWESS: %2.5f'%r2)
         
-    return (r2, y_pred, popt)
+    return y_pred
 
 def predict(x, y, days=1, extend=False, verbose=False):
     f = interpolate.interp1d(x, y, fill_value='extrapolate')
     if extend:
-        x_new = np.append(x, np.arange(1, 1+days) + x[-1])
-        y_new = np.append(y, f(np.arange(1, 1+days) + x[-1]))
+        x_new = np.append(x, np.arange(days) + x[-1] + 1)
+        y_new = np.append(y, f(np.arange(days) + x[-1] + 1))
         return (x_new, y_new)
     else:
         x_range = np.arange(1, 1+days) + x[-1]
